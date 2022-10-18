9c23689de8a8286504b1d683ab489a9235f60ca0
xiehui311 <513421699@qq.com>
2022-10-18 22:04:17 +0800

    添加文件

---
+++ src/main/java/com/hui/HelloGit.java
@@ -0,0 +1,13 @@
+package com.hui;
+
+/**
+ * @author Administrator
+ * @时间	2022年10月18日下午9:43:27
+ * @功能	
+ */
+public class HelloGit {
+
+	public static void main(String[] args) {
+		System.out.println("hello Git!");
+	}
+}
